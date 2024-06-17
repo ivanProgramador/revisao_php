5c28ae65a056bdcc5502b992561a902c92af5403
ivan <ivan-guita@hotmail.com>
2024-06-16 21:03:32 -0300

    funções aritmeticass

---
+++ exe009/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções matemáticas</title>
+</head>
+<body>
+    <?php
+      /*
+       valor absoluto retira a especificação positiva ou negativa de um numero mostrando somente ele 
+       como reultado por exemplo o valor absoluto de  -2 e igual a 2.
+       essa operação em php e feita usando a função:  abs();
+       
+       Funções matematicas de arredondamento
+
+       ceil()  -> arredonda para cima
+       floor() -> arredonda para baixo
+       round() -> faz o arredondamento aritmético
+
+       funções para calcular a hipotenusa hypot() 
+
+       intdiv() -> executa a divisão e retona somente numeros inteiros 
+       mim(1,2,3,4,5) -> paga o menor valor de um sequencia numerica, aqui ele retorna o 1
+       max(1,2,3,4,5) -> pega o menor valor de um sequencia numerica, aqui ele retona o 5
+
+       pow(5,2) -> essa é um função de potenciação no caso ela vai retonar 25 
+       
+       Funções trigonometricas 
+       sen() -> calcula seno
+       cos() -> calcula coseno
+       tan() -> calcula a tangente
+       
+       Função que calcula a raiz quadrada 
+       sqrt()
+       ainda não tem uma função para raiz cubica então para resolver calculos assim 
+       dá para usar a potenciação por exemplo:
+       aqui eu coloquei a divisão entre parenteses pra elçe dividir primiero e so depois elevar o 27 ao valor do resultado da divisão 
+       nesse caso ele vai retonar 3 
+       
+       $resultado =  27 ** (2/3);
+
+
+
+
+
+
+
+
+
+
+       
+       
+      */
+      
+    ?>
+    
+</body>
+</html>
