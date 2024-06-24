e331e81fe9e8f2135eefc2282f22e3d3725158e3
ivan <ivan-guita@hotmail.com>
2024-06-24 11:58:57 -0300

    conceito de aplicação web / fim da aula 4

---
+++ aula_4/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=?<>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+  /*
+   A base de um aplicação web são as ações
+   Requisição e Resposta.
+   
+   Requisição é uma solicitação em contexto de desenvolvimento e como pedir uma informação,
+   para que a resposta solicitada seja entregue ela deve vir por algum lugar que seria uma 
+   rota de acesso, dentro do computador pessoal quando queremos um arquivo temos que navegar 
+   até a pasta onde ele está clicar nele então ele sera, exibido, executado, reproduzido emfim 
+   dependendo do formato do arquivo basta clicar nele e você  terá a informação que deseja.
+
+   Uma aplicação web e a mesma coisa só que acada rota dessas fornece um arquivi que é a pagina html,
+   que foi requisitada então a pagina html que tem o php embutido que eo responsavel pela resposta 
+   o php não vai apararecer, somente os retornos dele aparecem para o cliente final.
+   
+   Mas isso não está restrito apenas ao php a resposta pode vir de um servidor más ela pode ser dada 
+   por  java,phyton,asp,c#,javascrpt ....,  que são linguens de porgramação usadas para processar a sua 
+   requisição e devolver a resposta com o conceito correto.
+
+
+   
+   
+
+   
+   
+  
+  */
+
+
+?>
+</body>
+</html>
