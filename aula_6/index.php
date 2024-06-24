91058cef13cf549362c048f24ce8fc6066129ac7
ivan <ivan-guita@hotmail.com>
2024-06-24 18:56:57 -0300

    declaração e nome de variaveis/ fim da aula 6

---
+++ aula_6/index.php
@@ -0,0 +1,20 @@
+<?php
+ /*
+  Variaveis em php 
+  
+  O php é um liguagem que exige algumas regras para declaração de variaveis como qualquer outr linguagem
+  
+  1 - Para que interpretador reconheça a variável declarada ela deve iniciar com o simbolo '$' exemplo: $nome
+
+  2 - O nome de um variável não pode conter espaços ou caracteres especiais(com excessão do undeline) exemplo: $_nome
+
+  3 - O php 8.2 até permite que o nome de variveis tenha ascentos porém isso não é uma boa prática porque como não se 
+      sabe em que versão o php que está no servidor web estará então é melhor seguir o padrão.   
+
+  4 - O php é uma linguegem case sensitive isso significa que ele considera letras maiusculas e minusculas na declaração
+      de variáveis ou seja ' $Nome ' é uma variável diferente de ''$nome'
+         
+ */
+
+
+?>
