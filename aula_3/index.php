16dd88e249e4cea6d4d63a250fde5aa85a4c9227
ivan <ivan-guita@hotmail.com>
2024-06-24 11:36:45 -0300

    saidas em php diferença entre echo e print /usando o php junto com html fim da aula 3

---
+++ aula_3/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!--
+ A diferença entre print e echo 
+  Antigamente para verificar se houve algum erro na impressão de algum conteudo
+  era usada a função print dessa forma: 
+    print('conteudo');
+  porém se acontecesse algum erro na impressão ela retonava o valor 0 se não ela retornava o valor 1 
+  isso ajudava aos desenvolvedores da epoca a identifcar possiveis erros de impressão no código
+  
+  O acho não tem essa funcção então ele é um forma mais rapida de imprimir visto que ele não 
+  precisa dar retorno, alem da linha impressa.
+   
+-->
+
+   <?php print '<h1> comando print </h1> '; ?>
+   <br>
+   <?php echo  '<p> comando echo </p>'; ?>
+    
+</body>
+</html>
