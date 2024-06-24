d015a79bc05f2b8873ed2e6e7e06501a409774a8
ivan <ivan-guita@hotmail.com>
2024-06-24 19:18:01 -0300

    impressão de valores de variáveis e tipagem  / fim da aula 7

--- aula_6/index.php
+++ aula_6/index.php
@@ -1,3 +1,14 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
  /*
   Variaveis em php 
@@ -13,8 +24,42 @@
 
   4 - O php é uma linguegem case sensitive isso significa que ele considera letras maiusculas e minusculas na declaração
       de variáveis ou seja ' $Nome ' é uma variável diferente de ''$nome'
-         
+
  */
 
+?>
+
+<?php
+  /*
+   As variveis em php recebem o tipo confiorma o valor que é atribuido a elas,
+   por exemplo a averivel nome é uma string porque eu coloquei uma cadeia de caracteres nela 
+   então eu posso dizer que o php é uma linguagem fracamente tipada.  
+
+  */
+
+  //string
+  $nome = 'Jorge Sant Ana';
+
+  //int
+  $idade = 29;
+
+  //float
+  $peso = 82.5;
+
+  //booleam
+  $fumante_s_n = false; //se for true retorna 1 se for false retorna vazio
 
 ?>
+
+
+<h1>Ficha de cadastro</h1>
+<p>nome: <?= $nome ?></p>
+<p>idade: <?= $idade ?></p>
+<p>peso: <?= $peso ?></p>
+<p>fumante: <?= $fumante_s_n ?></p>
+
+
+    
+</body>
+</html>
+
