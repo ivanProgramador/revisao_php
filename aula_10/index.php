c557cc3b7b9d83ccfa4db8a6e3a0bb0e7157c1ba
ivan <ivan-guita@hotmail.com>
2024-06-25 11:23:11 -0300

    opradores logicos e operadores de comparação / fim da aula 10

---
+++ aula_10/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Condicionais</title>
+</head>
+<body>
+
+ <?php
+ 
+    /* Operadores de comparação 
+
+     ==       ->  igual  a
+     ===      ->  identico a compara tanto o conteudo da variavel quanto o tipo 
+     != ou <> ->  diferente de
+     !==      ->  não identico a
+     <        ->  maior que
+     >        ->  menor que
+     <=       -> maior ou igual a 
+     >=       -> menor ou igual a 
+    
+     Operadores lógicos 
+
+     Operador E (AND ou &&) -> verdadeiro se todas as expressoões from verdadeiras 
+     Oprador  OU (OR ou || ) -> verdeiro se pelo menos uma das expressoões for verdeira
+     Operador Xor (XOR) ->  verdeiro se uma das expressoões for verdeira, mas não ambas
+     Operador  ! -> Inverte o resultado da expressão 
+
+    */
+
+
+
+
+
+ 
+ ?>
+    
+</body>
+</html>
