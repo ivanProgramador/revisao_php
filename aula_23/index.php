346dc45678ef6b9466dacccec811de2c3c38adcf
ivan <ivan-guita@hotmail.com>
2024-07-04 09:16:43 -0300

    pesquisa em arrays / final da aula 22

---
+++ aula_23/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesquisando em arrays</title>
+</head>
+<body>
+
+ <?php
+  
+
+  $lista_frutas = array('Abacate','Banana','Caju','Uva');
+
+  /*
+    usando o metodo in_array('Abacate',$lista_frutas);
+    esse metodo recebe 2 parametros 
+     1 - oque ele vai pesquisar 
+     2 - o array onde ele vai pesquisar 
+    ele retona 'true' se ele encontrar e 'false' caso ele não encontre 
+    a abusca dele e case sensitive 
+    
+    usando o metodo array_search('Abacaxi',$lista_frutas);
+    esse metodo recebe 2 parametros 
+     1 - oque ele vai pesquisar 
+     2 - o array onde ele vai pesquisar 
+    o esse metdo retorna o numero doiindice onde o dado pesauisado está 
+    então nesse caso o retorno seria 0 porque abacaxi esta no indice 0 
+    caso ele não encontre ele vai retonar null
+
+    A pesquisa em arrays multidimensionais exisge que seja feeito um loop para que essas funçãoes sejam
+    executadas em acada indice 
+
+
+
+
+  */
+
+  
+
+
+
+
+ 
+ 
+ ?>
+    
+</body>
+</html>
