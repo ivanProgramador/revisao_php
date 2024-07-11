0a36fedbcffc96527d8a89b7457d2f7b1c09b827
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-07-10 21:09:44 -0300

    praticando com loops/final da aula 28

--- aula_28/index.php
+++ aula_28/index.php
@@ -10,7 +10,15 @@
 
    <?php 
    
-     $registros = array('Comercio de ferias','Publico animado','Superlotação turisca','Como está o Dolar ?');
+     $registros = array(
+        array('Titulo'=>'Titulo noticia 1','Conteudo'=>'Conteudo noticia 1'),
+        array('Titulo'=>'Titulo noticia 2','Conteudo'=>'Conteudo noticia 2'),
+        array('Titulo'=>'Titulo noticia 3','Conteudo'=>'Conteudo noticia 3'),
+        array('Titulo'=>'Titulo noticia 1','Conteudo'=>'Conteudo noticia 1'),
+        array('Titulo'=>'Titulo noticia 2','Conteudo'=>'Conteudo noticia 2'),
+        array('Titulo'=>'Titulo noticia 3','Conteudo'=>'Conteudo noticia 3')
+       
+     );
 
      /*
      Para usar um loop é necessario ter uma variavel que controla essa repetição
@@ -26,6 +34,7 @@
       enquanto $idx for menor que 4 continue executando  a acada loop ele adicona mais um ao $idx 
      */
 
+     /*
      while($idx < 4){
 
         echo $registros[$idx]; //a cada loop o valor do $idx recebe 1 então com base novalor dele eu faço uma conmsulta nos indices do array 
@@ -37,6 +46,49 @@
         //fazendo o loop mostrar uma lista na tela
          
      }
+      */
+
+      /*
+       Arrays multidimensionais  
+      */
+
+      //para contar de forma dinamica os indices existentes dentro de um array exiSte a função count() eu vou usar ela para retornar para min quantos indices existem dentro do array 
+      
+    //  echo 'O Array possui : '.count($registros).' indices <br>';
+
+
+      //EXECUTANDO O WhILE COM BASE NO VALOR CONTADO DE INDICES 
+     /* while($idx < count($registros)){
+
+         echo'<h3>'.$registros[$idx]['Titulo'].'</h3>'; // mostrando o titulo no indice titulo 
+         echo'<p>'.$registros[$idx]['Conteudo'].'</p>'; // mostrando o conteudo no indice conteudo 
+         echo'<br>';
+
+         $idx ++; //] acada loop o $idx recebe 1 então cada inice e consultado com base no valor dele a cada loop 
+
+           
+      }
+         
+      */
+
+      for($idx = 0; $idx < count($registros);$idx++){
+
+         echo'<h3>'.$registros[$idx]['Titulo'].'</h3>'; // mostrando o titulo no indice titulo 
+         echo'<p>'.$registros[$idx]['Conteudo'].'</p>'; // mostrando o conteudo no indice conteudo 
+         echo'<hr>';
+
+          
+
+         
+
+
+      }
+
+     
+
+
+      
+      
      
    
    ?>
